be156780a8579f400f2e45e8aafc0b456766d01c
Mattia <matpolli93@gmail.com>
2021-03-26 20:02:51 +0100

    Small fixes

--- js/script.js
+++ js/script.js
@@ -9,19 +9,73 @@ const app = new Vue(
             newItem: "",
             usList: [
                 "Alabama"
+            ],
+            usListComplete: [ 
+                "Alabama",
+                "Alaska",
+                "Arizona",
+                "Arkansas",
+                "California",
+                "Colorado",
+                "Connecticut",
+                "Delaware",
+                "Florida",
+                "Georgia",
+                "Hawaii",
+                "Idaho",
+                "Illinois",
+                "Indiana",
+                "Iowa",
+                "Kansas",
+                "Kentucky",
+                "Louisiana",
+                "Maine",
+                "Maryland",
+                "Massachusetts",
+                "Michigan",
+                "Minnesota",
+                "Mississippi",
+                "Missouri",
+                "Montana",
+                "Nebraska",
+                "Nevada",
+                "New Hampshire",
+                "New Jersey",
+                "New Mexico",
+                "New York",
+                "North Carolina",
+                "North Dakota",
+                "Ohio",
+                "Oklahoma",
+                "Oregon",
+                "Pennsylvania",
+                "Rhode Island",
+                "South Carolina",
+                "South Dakota",
+                "Tennessee",
+                "Texas",
+                "Utah",
+                "Vermont",
+                "Virginia",
+                "Washington",
+                "West Virginia",
+                "Wisconsin",
+                "Wyoming"
             ]
         },
         methods: {
-            deleteItemList: function(itemList) {
-                this.usList.splice(itemList, 1);
+            deleteItemList: function(itemIndex) {
+                this.usList.splice(itemIndex, 1);
             },
             addItemList: function() {
-                if (this.newItem !== "") {
+
+                if (this.usList.length == this.usListComplete.length) {
+                    alert("Congratulations, You Won! 🇺🇸");
+                } else if ((this.newItem !== "") && (this.usListComplete.includes(this.newItem))) {
                     this.usList.push(this.newItem);
                     this.newItem = "";
-                }
+                }                
             }
         } 
     }
 );
-
