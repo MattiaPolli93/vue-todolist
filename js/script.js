02bff9cd09a7d631c794c7d038d94fca800241af
Mattia <matpolli93@gmail.com>
2021-03-26 16:17:49 +0100

    Add style and item-remove functionality

--- js/script.js
+++ js/script.js
@@ -6,10 +6,24 @@ const app = new Vue(
     {
         el: "#app",
         data: {
-
+            newItemList: "",
+            usList: [
+                "Alabama",
+                "Alaska",
+                "Arizona",
+                "Arkansas",
+                "California",
+                "Colorado",
+                "Connecticut",
+                "Delaware",
+                "Florida",
+                "Georgia"
+            ],
         },
-        methods:{
-            
-        }
+        methods: {
+            deleteItemList: function(itemList) {
+                this.usList.splice(itemList, 1);
+            },            
+        } 
     }
 );
